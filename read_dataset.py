31a920e00aa01d6646b8c65ab3af9fdf2aab435b
Xatyrian <xentsc2@gmail.com>
2017-10-30 14:59:17 +0100

    Added read_dataset.py to extract data from dataset

---
+++ read_dataset.py
@@ -0,0 +1,13 @@
+import os
+from scipy import misc 
+
+def load_dataset(filename, path, **args):
+    dataset = []
+    with open(filename,"r") as file:
+        for line in file:
+            print(line)
+            imgFile, x, y = line.split(" ")
+            img = misc.imread(os.path.join(path, imgFile), **args)
+            dataset.append([img, (int(x), int(y))])
+    
+    return dataset            
